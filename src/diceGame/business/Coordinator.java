bdd7578742f7ae19899f28eb1d384802643d9d77
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2024-07-30 09:15:40 +0100

    Add generateThrow sortThrow and printThrow methods

--- src/diceGame/business/Coordinator.java
+++ src/diceGame/business/Coordinator.java
@@ -3,8 +3,7 @@ package diceGame.business;
 import diceGame.data.DAO;
 import diceGame.data.DAOImplementation;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Coordinator {
     private DAO dao;
@@ -12,6 +11,8 @@ public class Coordinator {
     private Validation validator;
     private List<Player> players;
     private int currentPlayer;
+    private int round;
+    private ArrayList<Integer> currentThrow;
 
     public Coordinator() {
         this.dao = new DAOImplementation();
@@ -19,9 +20,18 @@ public class Coordinator {
         this.players = dao.getPlayers();
         this.validator = new Validation();
         this.currentPlayer = 0;
+        this.round = 0;
+        this.currentThrow = new ArrayList<>();
     }
 
     public void startGame () {
+        while (round <= 3) {
+            startRound();
+            round++;
+        }
+    }
+
+    public void startRound() {
         System.out.println();
         System.out.println(getPlayerName(currentPlayer));
         System.out.println(printMessage1(players.get(currentPlayer)));
@@ -33,7 +43,38 @@ public class Coordinator {
             System.out.println(printMessage2());
             input = scanner.nextLine().trim();
         }
+        this.currentThrow = generateThrow(players.get(currentPlayer));
+        System.out.print("Throw:  ");
+        System.out.print(printThrow(currentThrow));
+        System.out.println();
+        System.out.print("Sorted: ");
+        sortThrow(currentThrow);
+        System.out.print(printThrow(currentThrow));
 
+
+    }
+
+    public ArrayList<Integer> generateThrow(Player player) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < player.getDiceLeft(); i++) {
+            temp.add(rand.nextInt(6) + 1);
+        }
+        return temp;
+    }
+
+    public void sortThrow(ArrayList<Integer> temp) {
+        Collections.sort(temp, Collections.reverseOrder());
+    }
+
+    public StringBuilder printThrow(ArrayList<Integer> temp) {
+        StringBuilder diceList = new StringBuilder();
+        for (int i = 0; i < temp.size(); i++) {
+            diceList.append("[ ");
+            diceList.append(temp.get(i));
+            diceList.append(" ] ");
+        }
+        return diceList;
     }
 
     public boolean runValidation1(String input) {
