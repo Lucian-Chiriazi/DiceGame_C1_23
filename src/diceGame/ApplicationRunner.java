a88347f114cbf3008b6ad645dd240d957e4cb835
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2024-08-07 12:13:02 +0100

    Add load method for DAOImplementation to help with code reusability

--- src/diceGame/ApplicationRunner.java
+++ src/diceGame/ApplicationRunner.java
@@ -3,7 +3,7 @@ package diceGame;
 import diceGame.business.Coordinator;
 import diceGame.presentation.View;
 
-public class ApplicaionRunner {
+public class ApplicationRunner {
     public static void main(String[] args) {
         Coordinator coordinator = new Coordinator();
         View view = new View(coordinator);
--- src/diceGame/data/DAOImplementation.java
+++ src/diceGame/data/DAOImplementation.java
@@ -8,13 +8,22 @@ import java.util.List;
 
 public class DAOImplementation implements DAO {
 
+    private List<Player> players;
+
+    public DAOImplementation() {
+         this.players = new ArrayList<>();
+         load();
+    }
+
     public List<Player> getPlayers() {
-        List<Player> players = new ArrayList<>();
+
+        return players;
+    }
+
+    private void load() {
         players.add(new Player("Player 1"));
         players.add(new Player("Player 2"));
         players.add(new Player("Player 3"));
-
-        return players;
     }
 
 }
